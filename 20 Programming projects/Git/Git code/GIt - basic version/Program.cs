832eefbe1f166692210b0b95efafd3dfba1d011e
Jivko03 <47494058+Jivko03@users.noreply.github.com>
2025-08-03 00:41:19 +0300

    Add files via upload
    this is the skeleton of the sudo-git in C#

---
+++ 20 Programming projects/Git/Git code/GIt - basic version/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIt___basic_version
+{
+    internal class Program
+    {
+        const string joe = "joe";
+        const string joeinit = "joe init";
+        const string joeclone = "joe clone";
+        const string joeadd = "joe add";
+        const string joecommit = "joe commit";
+        static void Main(string[] args)
+        {
+            string commandInput = Console.ReadLine();
+            string path_to_create = @"C:\Users\Tamuring\Desktop\20 Programming projects\Git\Git code\template made projects with my sudo-git\.joe";
+            string path_to_clone = @"C:\Users\Tamuring\Desktop\20 Programming projects\Git\Git code\template made projects with my sudo-git";
+            switch (commandInput)
+            {
+                case "joe init":
+                    if 
+                        (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                        Console.WriteLine("Directory created successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Directory already exists.");
+                    }
+                    break;
+
+                case "joe clone":
+                    if
+                        (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                        Console.WriteLine("Directory created successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Directory already exists.");
+                    }
+                    break;
+
+                case "joe add":
+                    Console.WriteLine("Failed measurement.");
+                    break;
+                case "joe commit":
+                    Console.WriteLine("Failed measurement.");
+                    break;
+
+                default:
+                    Console.WriteLine($"Measured value is {measurement}.");
+                    break;
+            }
+        }
+    }
+}
